3d999597c883ea504703b8785d6dfb3421d1233a
char5742 <fujino@stellarcode.jp>
2025-03-05 21:07:47 +0900

    feat: 🎸認証ミドルウェアの改善とリダイレクト処理の追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,35 @@
 import { defineMiddleware, sequence } from "astro:middleware";
+import "./store";
+import { setAccountStorage } from "./store";
 
 /**
  * 認証ミドルウェア
  */
 const authenticate = defineMiddleware(async (context, next) => {
   // ログイン、サインアップページへのアクセスは許可
-  // if (
-  //   context.url.pathname.startsWith("/_actions/login") ||
-  //   context.url.pathname.startsWith("/_actions/signup")
-  // ) {
-  //   return next();
-  // }
-  // const isAuthenticated = await context.session?.has("accountId");
+  if (
+    context.url.pathname.startsWith("/_actions/login") ||
+    context.url.pathname.startsWith("/_actions/signup")
+  ) {
+    return next();
+  }
+  const accountId = await context.session?.get("accountId");
+  const isAuthenticated = !!accountId;
 
-  // // 認証されていない場合は、ログインページにリダイレクト
-  // if (!isAuthenticated && !context.url.pathname.startsWith("/auth/")) {
-  //   return context.redirect("/auth/login");
-  // }
-  // // もし認証済みで、ログイン、サインアップページにアクセスした場合は、トップページにリダイレクト
-  // if (isAuthenticated && context.url.pathname.startsWith("/auth/")) {
-  //   return context.redirect("/");
-  // }
+  // 認証されていない場合は、ログインページにリダイレクト
+  if (!isAuthenticated) {
+    // 既にログインページにいる場合は、次のミドルウェアまたはハンドラに進む
+    if (context.url.pathname.startsWith("/auth/")) return next();
+    return context.redirect("/auth/login");
+  }
+  // もし認証済みで、ログイン、サインアップページにアクセスした場合は、トップページにリダイレクト
+  if (context.url.pathname.startsWith("/auth/")) {
+    return context.redirect("/");
+  }
+
+  // 認証済みの場合は、次のミドルウェアまたはハンドラに進む
+  context.locals.accountId = accountId;
+  setAccountStorage(accountId);
   return next();
 });
 
